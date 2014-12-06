2862754cd45f2fd3e52b750d160b4162b1d7ecac
jrivasluna <j.rivasluna@gmail.com>
2014-12-06 08:37:34 -0600

    Manuales
    Estos son los manuales de usuarios, SOLO EN EL MODULO DE VENTAS ESTAN
    REFRENCIADAS!

---
+++ manuales/manuales.php
@@ -0,0 +1,182 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Ayuda</title>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="css/reset.css" type="text/css" media="screen">
+    <link rel="stylesheet" href="css/style.css" type="text/css" media="screen">
+    <link rel="stylesheet" href="css/layout.css" type="text/css" media="screen">
+    <style type="text/css">
+    .titulo {
+	font-family: "Comic Sans MS", cursive;
+	font-size: 16px;
+	color: #000;
+	text-decoration: underline blink;
+}
+    .LINKS {
+	font-family: "Comic Sans MS", cursive;
+	font-size: 16px;
+	color: #00F;
+}
+    .TEXTO {
+	font-family: Verdana, Geneva, sans-serif;
+	font-size: 12px;
+	color: #000;
+}
+    </style>
+    <script src="js/jquery-1.7.1.min.js" type="text/javascript"></script>
+    <script src="js/cufon-yui.js" type="text/javascript"></script>
+    <script src="js/cufon-replace.js" type="text/javascript"></script> 
+    <script src="js/Dynalight_400.font.js" type="text/javascript"></script>
+    <script src="js/FF-cash.js" type="text/javascript"></script>  
+	<!--[if lt IE 8]>
+    <div style=' clear: both; text-align:center; position: relative;'>
+        <a href="http://windows.microsoft.com/en-US/internet-explorer/products/ie/home?ocid=ie6_countdown_bannercode">
+        	<img src="http://storage.ie6countdown.com/assets/100/images/banners/warning_bar_0000_us.jpg" border="0" height="42" width="820" alt="You are using an outdated browser. For a faster, safer browsing experience, upgrade for free today." />
+        </a>
+    </div>
+	<![endif]-->
+    <!--[if lt IE 9]>
+   		<script type="text/javascript" src="js/html5.js"></script>
+	<![endif]-->
+</head>
+<body id="page6">
+	<!--==============================header=================================-->
+    <header>
+    	<div class="row-top">
+        	<div class="main">
+            	<div class="wrapper">
+                	<h1><a href="../index_ALFINTE.html"><img src="Captura.PNG" width="218" height="141"><span>Alfinte S.</span><span>A de C.V.</span></a>                    </h1>
+                	<nav>
+                      <ul class="menu">
+                            
+                       
+                            <li></li>
+                            <li></li>
+                            <li></li>
+                      </ul>
+                  </nav>
+                </div>
+            </div>
+        </div>
+        <div class="row-bot">
+        	<div class="row-bot-bg">
+            	<div class="main">
+                	<h2><span>Bienvenido a la Seccion de Ayuda.</span></h2>
+                </div>
+            </div>
+        </div>
+    </header>
+    
+	<!--==============================content================================-->
+    <section id="content"><div class="ic"></div>
+        <div class="main">
+            <div class="wrapper">
+              <article class="col-2">
+           	    <h3 class="p1">Seleccione una opción</h3>
+                
+                
+                
+<ul type="spare">
+            <li class="titulo">Modulo Citas</li>
+  <li class="titulo"></li>
+           
+            <li class="TEXTO"><span class="TEXTO">El Modulo Cita proporciona las interacciones en el cual el usuario puede:
+            </span></li>
+            <ol type=a>
+		  <li class="TEXTO"><span class="TEXTO">--&gt;Consultar citas pendientes</span></li>
+			  <li class="TEXTO"><span class="TEXTO">--&gt;Confirmar citas</span></li>
+			  <li class="TEXTO"><span class="TEXTO">--&gt;Crear citas</span></li>
+			  <li class="TEXTO"><span class="TEXTO">--&gt;Crear citas localmente</span></li>
+              <li><a href="MANUAL DE CITAS.PDF" class="LINKS">download</a></li>
+  </ol>
+        	
+		<br>
+  <li class="titulo">Modulo Taller</li>
+  <ol>
+    <li class="TEXTO">El Modulo taller proporciona las interacciones en el cual el usuario puede: </li>
+    <ol type=a>
+      <li class="TEXTO">--&gt;Asignar Orden de trabajo</li>
+      <li class="TEXTO">--&gt;Ver estado de orden de trabajo</li>
+      <li class="TEXTO">--&gt;Solicitud de materiales</li>
+      <li class="TEXTO">--&gt;Asignacion de instalacion</li>
+    </ol>
+    <li></li>
+  </ol>
+				
+  <p><a href="" class="LINKS">download</a><br>
+  </p>
+		<li class="titulo">Modulo Inventario</li>
+		<ol>
+          <li></li>
+  </ol>
+		<ol>
+		  <li class="TEXTO">El Modulo Inventario proporciona las interacciones en el cual el usuario puede: </li>
+		  <ol type=a>
+		    <li class="TEXTO">--&gt;Consultar articulos</li>
+		    <li class="TEXTO">--&gt;Traslados de articulos</li>
+		    <li class="TEXTO">--&gt;Modificar articulos</li>
+		    <li class="TEXTO"></li>
+	      </ol>
+		  <li></li>
+  </ol>
+        <p><a href="MANUAL INVENTARIO.PDF" class="LINKS">download</a></p>
+		<li class="titulo">Modulo Administración</li>
+		<ol>
+          <li></li>
+                   <ol>
+                     <li class="TEXTO">El Modulo Administracion proporciona las interacciones en el cual el usuario puede: </li>
+                     <ol type=a>
+                       <li class="TEXTO">--&gt;Crear usuarios</li>
+                       <li class="TEXTO">--&gt;Asignar roles del usuario</li>
+                     </ol>
+                     <li></li>
+                   </ol>
+                   <p><a href="MANUAL ADMINISTRACION.PDF" class="LINKS">download</a></p>
+                   <li></li>
+				  
+		   				   <li>
+		   				     <p>&nbsp;</p>
+                           </li>
+</ul>  
+                
+                
+                
+                
+                
+                
+                
+           	    <!--<p>&nbsp;</p>
+           	    <p>
+           	      <label><span class="text-form">Manuel de Usuaruo</span></label>
+           	    </p>
+           	    <p>&nbsp;</p>
+           	    <p>
+           	      <label><span class="text-form"> Manual Tecnico     	          </span></label>
+           	    </p>
+           	    <p>&nbsp;</p>
+           	    <p>
+           	      <label><span class="text-form">Guia de instalacion </span></label>
+       	        </p>
+           	    <p>&nbsp;</p>
+           	    <p>
+           	      <label>         	        </label>
+           	    </p>
+           	    <p>&nbsp;</p>
+           	    <p>&nbsp;</p>
+           	    <p>&nbsp;</p>
+           	    <p>&nbsp;</p>
+           	    <form id="contact-form" method="post" enctype="multipart/form-data">
+           	      <fieldset>
+           	      </fieldset>						
+                </form>
+              </article>
+          </div>
+        </div>
+    </section>
+    
+	<!--==============================footer=================================-->
+<footer></footer>
+    <script type="text/javascript"> Cufon.now(); </script>
+</body>
+</html>
